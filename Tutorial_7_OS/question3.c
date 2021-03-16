f690880fadb446cebd2690421e0d21b490cf9802
kavanc <kavanchandra@outlook.com>
2021-03-15 23:46:13 -0400

    q3 and q4 added

---
+++ Tutorial_7_OS/question3.c
@@ -0,0 +1,23 @@
+#include <stddef.h> 
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <stdbool.h> 
+#include <unistd.h> 
+#include <signal.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void) {
+    pid_t pid;
+    pid = fork();
+
+    if(pid == 0){
+        
+        execl("./process", NULL);
+    }
+    else{
+        sleep(5);
+        kill(pid, SIGINT);
+    }
+
+} 
